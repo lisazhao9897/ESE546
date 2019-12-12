effb930204758d96b49486cea2c17ebffbfc582e
lisazhao9897 <45127102+lisazhao9897@users.noreply.github.com>
2019-12-12 15:23:26 -0500

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+#-*-coding=utf-8 -*-
+import argparse
+import json
+import sys
+
+parser = argparse.ArgumentParser(description='The year of COCO :2014 or 2017')
+parser.add_argument('--year', default=None,type=str, help='2014 or 2017')
+args = parser.parse_args()
+
+year = args.year
+COCO_Year = ['2014', '2017']
+if year not in COCO_Year:
+    raise Exception("You need to choose the year of COCO:2014 or 2017")
+
+className = {
+            1:'person'
+            }
+
+classNum = [1]
+
+def writeNum(Num):
+    with open("annotations/COCO_train"+year+".json","a+") as f:
+        f.write(str(Num))
+
+def writeNum2(Num):
+    with open("annotations/COCO_val"+year+".json","a+") as f:
+        f.write(str(Num))
+
+# train 
+inputfile = []
+inner = {}
+
+with open("annotations/instances_train"+year+".json","r+") as f:
+    allData = json.load(f)
+    data = allData["annotations"]
+
+for i in data:
+    if(i['category_id'] in classNum):
+        inner = {
+            "filename": str(i["image_id"]).zfill(6),
+            "name": className[i["category_id"]],
+            "bndbox":i["bbox"]
+        }
+        inputfile.append(inner)
+inputfile = json.dumps(inputfile)
+writeNum(inputfile)
+
+# val 
+inputfile2 = []
+inner2 = {}
+
+with open("annotations/instances_val"+year+".json","r+") as f:
+    allData = json.load(f)
+    data = allData["annotations"]
+
+for i in data:
+    if(i['category_id'] in classNum):
+        inner2 = {
+            "filename": str(i["image_id"]).zfill(6),
+            "name": className[i["category_id"]],
+            "bndbox":i["bbox"]
+        }
+        inputfile2.append(inner2)
+inputfile2 = json.dumps(inputfile2)
+writeNum2(inputfile2)
---
+++ remove.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import os
+import argparse
+parser = argparse.ArgumentParser(description='The year of COCO :2014 or 2017')
+parser.add_argument('--year', default=None,type=str, help='2014 or 2017')
+args = parser.parse_args()
+
+year = args.year
+COCO_Year = ['2014', '2017']
+if year not in COCO_Year:
+    raise Exception("You need to choose the year of COCO:2014 or 2017")
+
+# train 
+nameStr = []
+
+with open("annotations/COCO_train"+year+".json","r+") as f:
+    data = json.load(f)
+
+if year == '2014':
+    for i in data:
+        imgName = "COCO_train2014_" + str(i["filename"]).zfill(12) + ".jpg"
+        nameStr.append(imgName)
+elif year == '2017':
+    for i in data:
+        imgName = str(i["filename"]).zfill(12) + ".jpg"
+        nameStr.append(imgName)
+
+nameStr = set(nameStr)
+
+path = "train"+year+"/"
+for file in os.listdir(path):
+    if(file not in nameStr):
+        os.remove(path+file)
+
+
+# val 
+nameStr2 = []
+
+with open("annotations/COCO_val"+year+".json","r+") as f:
+    data = json.load(f)
+
+if year == '2014':
+    for i in data:
+        imgName = "COCO_val2014_" + str(i["filename"]).zfill(12) + ".jpg"
+        nameStr2.append(imgName)
+elif year == '2017':
+    for i in data:
+        imgName = str(i["filename"]).zfill(12) + ".jpg"
+        nameStr2.append(imgName)
+
+nameStr2 = set(nameStr2)
+
+path = "val"+year+"/"
+for file in os.listdir(path):
+    if(file not in nameStr2):
+        os.remove(path+file)
